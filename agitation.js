71a217ae6d51f134c7e58007e21c6c0c637ec648
albrtcndo <73441227+albrtcndo@users.noreply.github.com>
2020-11-04 01:07:18 +0100

    animazion

--- agitation.js
+++ agitation.js
@@ -15,20 +15,44 @@ function shakeEventDidOccur () {
     //animazione su palla della verità
     //far apparire il messaggio
     //alert('shake!');
-    paginaMessaggio = $(".container-messaggio");
+    const paginaMessaggio = document.getElementById("container-messaggio");
     //controllo se esiste una frase aperta se sì chiudo
-    if (paginaMessaggio.children.length > 1) {
+    if (paginaMessaggio.children.length > 0) {
         console.log("c'era già un messaggio aperto, mo lo chiudiamo");
 
         //aggiungo animazione chiusura
+        var bgDisappear = $("#container-messaggio");
+        var messageDisappear = $("#container-messaggio .messaggio");
+        gsap.to(bgAppear, {
+            autoAlpha: 0,
+            duration: 2,
+            ease: Bounce. easeOut
+        },'+= 0.1')
+        gsap.to(messageDisappear, {
+            x: -400,
+            autoAlpha: 0,
+            transformOrigin: '50% 0%',
+            duration: 5,
+            ease: Bounce. easeOut
+        },'-= 0.1')
 
         paginaMessaggio.firstElementChild.remove();
+        console.log("elemento rimosso");
     }
 
+    var arrayFrasi = ["Un sorriso, un caffè, un buongiorno per te!",
+    "Se al mattino la tua casa profuma di caffè, sicuramente il tuo sarà un giorno da Re!",
+    "Le migliori frasi di vin disel"];
+
     var fraseFinta = "Un sorriso, un caffè, un buongiorno per te!";
+    var indiceAcaso = getRandomInt(0, 2);
+    console.log("questo è l'indice a caso generato");
+    console.log(indiceAcaso);
+
+    var fraseFinta2 = arrayFrasi[indiceAcaso];
     const fraseNew = document.createElement("p");
     fraseNew.classList.add("messaggio");
-    fraseNew.innerHTML = fraseFinta;
+    fraseNew.innerHTML = fraseFinta2;
 
     paginaMessaggio.appendChild(fraseNew);
 
@@ -36,7 +60,15 @@ function shakeEventDidOccur () {
     $("#palla-della-verita").addClass("shake-slow");
 
     //animazione messaggio
-    var messageAppear = $(".container-messaggio .messaggio");
+    var bgAppear = $("#container-messaggio");
+    var messageAppear = $("#container-messaggio .messaggio");
+    gsap.fromTo(bgAppear, {
+        autoAlpha: 0,
+    }, {
+        autoAlpha: 1,
+        duration: 2,
+        ease: Bounce. easeOut
+    },'+= 0.1')
     gsap.fromTo(messageAppear, {
         x: -400,
         display: "none",
@@ -52,4 +84,8 @@ function shakeEventDidOccur () {
         ease: Bounce. easeOut
     },'+= 0.1')
 
-}
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
--- cose_buone.js
+++ cose_buone.js
@@ -4,5 +4,10 @@ $( document ).ready(function() {
   if(window.outerWidth < 425) {
     $(".blocchettino-su-desktop").addClass("nascondi");
   }
-  
+  //test on click
+  $('body').click(function() {
+   // do something here
+   shakeEventDidOccur();
+  });
+
 });
